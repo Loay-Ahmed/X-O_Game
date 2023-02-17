68f4ede1cc7292614e138461555c779b25e0e610
Loay-Ahmed <loayabelgendy@gmail.com>
2023-02-17 13:01:33 +0200

    Updated and tested last version

--- x_o_game.py
+++ x_o_game.py
@@ -1,24 +1,25 @@
 """X-O Game"""
 
+
 table: list = [
-    " 1 ", " 2 ", " 3 ",
-    " 4 ", " 5 ", " 6 ",
-    " 7 ", " 8 ", " 9 "
+    "1", "2", "3",
+    "4", "5", "6",
+    "7", "8", "9"
 ]
 
 WINNER = False
 TURN = "X"
 
 while WINNER is not True:
-    print(table[0] + '|' + table[1] + '|' + table[2])
+    print(" " + table[0] + ' | ' + table[1] + ' | ' + table[2] + " ")
     print('---+---+---')
-    print(table[3] + '|' + table[4] + '|' + table[5])
+    print(" " + table[3] + ' | ' + table[4] + ' | ' + table[5] + " ")
     print('---+---+---')
-    print(table[6] + '|' + table[7] + '|' + table[8] + "\n")
+    print(" " + table[6] + ' | ' + table[7] + ' | ' + table[8] + " \n")
 
-    x = input("Enter the " + TURN + "\'s position: ")
-    if x.isdigit():
-        table[int(x)-1] = f' {TURN} '
+    x = input("Enter the " + TURN + "\'s position: \n")
+    if x.isdigit() and x in table:
+        table[int(x)-1] = TURN
     else:
         print("You didn\'t enter a number!!!")
         x = input("Enter the " + TURN + "\'s position: ")
@@ -39,12 +40,11 @@ while WINNER is not True:
         else:
             TURN = "X"
 
-print(table[0] + '|' + table[1] + '|' + table[2])
+print(" " + table[0] + ' | ' + table[1] + ' | ' + table[2] + " ")
 print('---+---+---')
-print(table[3] + '|' + table[4] + '|' + table[5])
+print(" " + table[3] + ' | ' + table[4] + ' | ' + table[5] + " ")
 print('---+---+---')
-print(table[6] + '|' + table[7] + '|' + table[8])
-
+print(" " + table[6] + ' | ' + table[7] + ' | ' + table[8] + " ")
 WIN_TXT = f' The WINNER is {TURN} '
 
 print (f'\n{WIN_TXT:=^40}\n')
